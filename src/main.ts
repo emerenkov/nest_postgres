59a6e0d43bf4e5fc6cc47bc8caa7fa5d1017d7b8
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2024-05-28 20:45:47 +0200

    feat: do dto; do swagger;

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 
 async function start() {
   const PORT = process.env.PORT || 5001;
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BACKEND NEST + POSTGRES')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .addTag('Evgeniy Merenkov')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document)
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
